e1c4709f12491286d5a6c74acdef4b3445da5167
Facundo Olano <facundo.olano@gmail.com>
2022-07-22 18:58:12 -0300

    improve readme template and script

--- gen_readme.py
+++ gen_readme.py
@@ -6,34 +6,58 @@
 
 import yaml
 
-# load paper specs
-with open('papers.yml') as file_:
-    papers = yaml.safe_load(file_)
 
-# build table of contents
-papers_toc = ''
-for paper in papers:
-    papers_toc += f'1. **{paper["title"]}**. [{paper["author"]} ({paper["year"]})]({paper["link"]}).\n'
-    for related in sorted(paper.get('related', []), key=lambda p: p["year"]):
-        papers_toc += f'    * {related["title"]}. [{related["author"]} ({related["year"]})]({related["link"]}).\n'
+def load_papers():
+    def markdown(paper, bold=False):
+        title = f'**{paper["title"]}**' if bold else paper['title']
+        return f'{title}. [{paper["author"]} ({paper["year"]})]({paper["link"]}).\n'
 
-# build full flat list
-for paper in papers:
-    for rel in paper.get('related', []):
-        papers.append(rel)
+    with open('papers.yml') as file_:
+        papers = yaml.safe_load(file_)
 
-papers.sort(key=lambda p: p['year'])
-papers_full = ''
-for n, paper in enumerate(papers):
-    papers_full += f'1. {paper["title"]}. [{paper["author"]} ({paper["year"]})]({paper["link"]})\n'
+    for paper in papers:
+        paper['markdown'] = markdown(paper, bold=True)
+        for related in paper.setdefault('related', []):
+            related['markdown'] = markdown(related, bold=False)
+
+        paper['related'].sort(key=lambda p: p['year'])
+
+    return papers
 
 
-# inject into readme
-with open('README.md.template') as template:
-    # this (and the previous markdown) could be rewritten to use some templating engine,
-    # but at this size I find it simpler to format manually
-    output = template.read().\
-        replace('{{ PAPERS_TOC }}', papers_toc).\
-        replace('{{ PAPERS_FULL }}', papers_full)
+def render_readme(papers):
+    with open('README.md.template') as template:
+        readme = template.read()
 
-print(output)
+    # Full nested list
+    papers_full = ''
+    for paper in papers:
+        papers_full += '1. ' + paper['markdown']
+        for related in paper['related']:
+            papers_full += '    * ' + related['markdown']
+    readme = readme.replace('{{ PAPERS_FULL }}', papers_full)
+
+    # top-level only
+    papers_top = ''
+    for paper in papers:
+        papers_top += '1. ' + paper['markdown']
+    readme = readme.replace('{{ PAPERS_TOC }}', papers_top)
+
+    # chronological flat list
+    all_papers = []
+    for paper in papers:
+        all_papers.append(paper)
+        all_papers += paper['related']
+
+    papers_sorted = ''
+    for paper in sorted(all_papers, key=lambda p: p['year']):
+        papers_sorted += '1. ' + paper['markdown']
+    readme = readme.replace('{{ PAPERS_SORTED }}', papers_sorted)
+
+    return readme
+
+
+if __name__ == '__main__':
+    papers = load_papers()
+    output = render_readme(papers)
+    print(output)
